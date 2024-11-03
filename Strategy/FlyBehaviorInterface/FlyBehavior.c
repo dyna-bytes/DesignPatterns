9ef6f6d0f8b013821860ac83e7e22745b41f21a7
Jihyuk Park <jetpack@LAPTOP-8IJEQCQD>
2024-11-03 15:51:12 +0900

    [DIR] change path
    Signed-off-by: Jihyuk Park <jetpack@LAPTOP-8IJEQCQD>

--- Strategy/FlyBehaviorInterface/FlyBehavior.c
+++
@@ -1,7 +0,0 @@
-#include "FlyBehaviorInterface/FlyBehavior.h"
-
-void __initFlyBehavior(FlyBehavior *self,
-    void (*fly)(void))
-{
-    self->fly = fly;
-}
--- Strategy/QuackBehaviorInterface/QuackBehavior.c
+++
@@ -1,5 +0,0 @@
-#include "QuackBehaviorInterface/QuackBehavior.h"
-
-void __initQuackBehavior(QuackBehavior *self, void (*quack)(void)) {
-    self->quack = quack;
-}
